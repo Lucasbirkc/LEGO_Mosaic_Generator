6c2d2de9fb0b9fe3136e877dbccf039d0d2c7e49
Lucch20 <Lucch20@student.sdu.dk>
2025-11-01 19:48:15 +0100

    Setup Mosaic class structure

--- mosaic.py
+++ mosaic.py
@@ -4,3 +4,30 @@ from pathlib import Path
 import numpy as np
 from PIL import Image, ImageDraw
 
+class MosaicGenerator():
+    def __init__(self):
+        pass
+
+    def _load_config(self):
+        pass
+
+    def _load_colors(self):
+        pass
+
+    def _find_closest_color(self):
+        pass
+
+    def _process_image(self):
+        pass
+
+    def _generate_preview(self):
+        pass
+
+    def _generate_shopping_list(self):
+        pass
+
+    def generate(self):
+        pass
+
+if __name__ == '__main__':
+    pass
